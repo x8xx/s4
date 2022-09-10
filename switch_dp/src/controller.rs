390e927cf6fe9cef700c27bd27142c39476c97f3
K.Tsukimoto <t.29.wuq@gmail.com>
2022-09-10 12:38:13 +0000

    wip

--- switch_dp/src/controller.rs
+++ switch_dp/src/controller.rs
@@ -4,7 +4,6 @@ use std::io::prelude::*;
 use crate::config::*;
 use crate::dpdk::dpdk_memory;
 use crate::worker::*;
-use crate::fib::cache::*;
 use crate::fib::header;
 use crate::fib::parser;
 use serde::Deserialize;
@@ -56,19 +55,19 @@ fn get_sample_dp_config_header() -> String {
             \"headers\": {
                 \"ethernet\": {
                     \"fields\": [48, 48, 16],
-                    \"used_fields\": []
+                    \"used_fields\": [0, 1, 2]
                 },
                 \"ipv4\": {
                     \"fields\": [4, 4, 8, 16, 16, 3, 13, 8, 8, 16, 32, 32],
-                    \"used_fields\": []
+                    \"used_fields\": [9, 10, 11]
                 },
                 \"tcp\": {
                     \"fields\": [16, 16, 32, 32, 4, 6, 6, 16 ,16, 16],
-                    \"used_fields\": []
+                    \"used_fields\": [0, 1]
                 },
                 \"udp\": {
                     \"fields\": [16, 16, 16],
-                    \"used_fields\": []
+                    \"used_fields\": [0, 1]
                 }
             }
         }
@@ -81,32 +80,37 @@ pub fn controller_start(switch_config: &SwitchConfig) {
     /* ------------------------
      * Header
      * --------------------------*/
+    println!("😟Loading Header");
     let dp_config_json = get_sample_dp_config_header();
     let dp_config: DpConfig = serde_json::from_str(&dp_config_json).unwrap();
     let hdr_def_num = dp_config.headers.len();
-
+    println!("{}", hdr_def_num);
     let hdr_defs = dpdk_memory::malloc::<header::Header>("header_definitions".to_string(), hdr_def_num as u32);
 
     for (i, hdr_conf) in dp_config.headers.iter().enumerate() {
         unsafe {
-            *hdr_defs.offset(i as isize) = header::Header::new(*hdr_conf.0, &hdr_conf.1.fields, &hdr_conf.1.used_fields);
+            *hdr_defs.offset(i as isize) = header::Header::new((&*hdr_conf.0).to_string(), &hdr_conf.1.fields, &hdr_conf.1.used_fields);
         }
     }
+    println!("👍Done");
+
 
     /* ------------------------
-     * parser
+     * Parser
      * --------------------------*/
+    println!("😟Loading Parser");
     let mut f = File::open(&switch_config.parser_path).unwrap();
     let metadata = std::fs::metadata(&switch_config.parser_path).unwrap();
     let mut parser_bin = vec![0;metadata.len() as usize];
     f.read(&mut parser_bin).unwrap();
 
     let llvm_compiler = LLVM::default();
-    let parser_store = wasmer::Store::new(&wasmer::Universal::new(llvm_compiler).engine());
+    // let parser_store = wasmer::Store::new(&wasmer::Universal::new(llvm_compiler).engine());
+    let parser_store = wasmer::Store::default();
     let parser_module = wasmer::Module::from_binary(&parser_store, &parser_bin).unwrap();
 
     let parser_fn_pkt_read = wasmer::Function::new_native(&parser_store, parser::wasm_pkt_read);
-    let parser_fn_extract_header = wasmer::Function::new_native(&parser_store, parser::wasm_pkt_read);
+    let parser_fn_extract_header = wasmer::Function::new_native(&parser_store, parser::wasm_extract_header);
     let parser_linear_memory = wasmer::Memory::new(&parser_store, wasmer::MemoryType::new(1, None, false)).unwrap();
     let parser_import_object = wasmer::imports! {
         "env" => {
@@ -118,19 +122,18 @@ pub fn controller_start(switch_config: &SwitchConfig) {
 
     let parser_instance = wasmer::Instance::new(&parser_module, &parser_import_object).unwrap();
     let fn_parse = parser_instance.exports.get_function("parse").unwrap();
+    println!("👍Done");
 
 
     /* ------------------------
-     * pipeline
+     * Pipeline
      * --------------------------*/
     let pipelines = dpdk_memory::malloc::<wasmer::Function>("pipelines".to_string(), 100);
     
 
 
-
-
     /* ------------------------
-     * rx core
+     * rx core (load balancer)
      * --------------------------*/
     let l1_cache_len = 65535;
     let l1_cache = dpdk_memory::malloc::<u8>("l1_cache".to_string(), l1_cache_len);
@@ -140,15 +143,15 @@ pub fn controller_start(switch_config: &SwitchConfig) {
     let lb_filter_len = 65535;
     let lb_filter = dpdk_memory::malloc::<u8>("lb_filter".to_string(), lb_filter_len);
 
-
     let mut fib_core_rings = Vec::new();
     fib_core_rings.push(dpdk_memory::Ring::new("fib1", 4096));
     fib_core_rings.push(dpdk_memory::Ring::new("fib2", 4096));
 
     let mut rx_start_args = rx::RxStartArgs {
         if_name: &switch_config.if_name,
-        hdrs,
-        parser: &fn_parse,
+        hdrs: hdr_defs,
+        hdrs_len: hdr_def_num as u32,
+        parser: fn_parse,
         l1_cache,
         lb_filter,
         fib_core_rings: &fib_core_rings,
@@ -156,14 +159,24 @@ pub fn controller_start(switch_config: &SwitchConfig) {
 
 
     /* ------------------------
-     * fib core
+     * cache core
      * --------------------------*/
     let mut fib_start_args =fib::FibStartArgs {
 
     };
 
+
+    /* ------------------------
+     * pipeline core
+     * --------------------------*/
+
+
+
     allocate_core_to_worker(switch_config, &mut rx_start_args, &mut fib_start_args);
 
 
+    loop {
+
+    }
     // run tcp
 }
--- switch_dp/src/worker/rx.rs
+++ switch_dp/src/worker/rx.rs
@@ -1,15 +1,19 @@
 use std::ffi::c_void;
 use std::mem::transmute;
+use std::ptr::null_mut;
 use crate::fib::*;
+use crate::fib::header::Header;
+use crate::fib::parser::WasmParserArgs;
 use crate::dpdk::dpdk_memory;
 use crate::dpdk::dpdk_port;
 use crate::dpdk::dpdk_eth;
 
-
 #[repr(C)]
 pub struct RxStartArgs<'a> {
     pub if_name: &'a str,
-    // pub parser: &'a parser::Parser<'a>,
+    pub hdrs: *const Header,
+    pub hdrs_len: u32,
+    pub parser: &'a wasmer::Function,
     pub l1_cache: *mut u8,
     pub lb_filter: *mut u8,
     pub fib_core_rings: &'a [dpdk_memory::Ring],
@@ -21,11 +25,20 @@ fn next_core() -> usize {
 }
 
 
-pub extern "C" fn rx_start(rx_start_args_ptr: *mut c_void) -> i32 {
-    println!("rx_start");
+fn create_l1_key(hdr_flag: i32) {
 
+}
+
+
+pub extern "C" fn rx_start(rx_start_args_ptr: *mut c_void) -> i32 {
+    println!("😟Loading Rx Core");
     let rx_start_args = unsafe {&*transmute::<*mut c_void, *mut RxStartArgs>(rx_start_args_ptr)};
     let if_name = &rx_start_args.if_name;
+
+    let hdrs = rx_start_args.hdrs;
+    let hdrs_len = rx_start_args.hdrs_len;
+    let parser = rx_start_args.parser;
+
     let l1_cache = &rx_start_args.l1_cache;
     let lb_filter = &rx_start_args.lb_filter;
     let fib_core_rings = &rx_start_args.fib_core_rings;
@@ -39,7 +52,27 @@ pub extern "C" fn rx_start(rx_start_args_ptr: *mut c_void) -> i32 {
 
     let mut random_next_core = 0;
 
-    println!("start loop");
+    let mut parser_args: [wasmer::Value;3] = [wasmer::Value::I64(0), wasmer::Value::I32(0), wasmer::Value::I64(0)];
+    // let mut parser_args: [wasmer::Value;1] = [wasmer::Value::I64(0)];
+    let packet_batch_num = 32;
+    let parsed_hdrs = dpdk_memory::malloc::<*mut (u8, *mut Header)>("rx_parsed_hdrs".to_string(), packet_batch_num);
+    for  i in 0..packet_batch_num {
+        unsafe {
+            let parsed_hdr_name = format!("rx_parsed_hdr_{}", i);
+            *parsed_hdrs.offset(i as isize) = dpdk_memory::malloc::<(u8, *mut Header)>(parsed_hdr_name, hdrs_len);
+        }
+
+    }
+
+    let mut wasm_parser_args = WasmParserArgs {
+        hdrs: hdrs as *mut Header,
+        parsed_hdr: null_mut(),
+        size: 0,
+    };
+    let wasm_parser_args_ptr = &mut wasm_parser_args as *mut WasmParserArgs;
+    println!("👍Rx Core Ready!");
+
+    println!("🚀Launch Switch Port {}", if_name);
     loop {
         let rx_count = pp.rx();
         if rx_count <= 0 {
@@ -47,6 +80,24 @@ pub extern "C" fn rx_start(rx_start_args_ptr: *mut c_void) -> i32 {
         }
         for i in 0..rx_count {
             let pkt = pp.get_packet(i);
+            
+            wasm_parser_args.size = 0;
+            unsafe {
+                wasm_parser_args.parsed_hdr = *parsed_hdrs.offset(i as isize);
+            }
+
+            parser_args[0] = wasmer::Value::I64(pkt.as_ptr() as i64);
+            parser_args[1] = wasmer::Value::I32(pkt.len() as i32);
+            parser_args[2] = wasmer::Value::I64(wasm_parser_args_ptr as i64);
+            let parse_result= parser.call(&parser_args);
+            // println!("ok2");
+            match parse_result {
+                Err(_) => continue,
+                _ => {},
+            };
+
+            println!("check");
+            continue;
 
             let l1_key = &pkt[0..112];
             let l1_hash = murmurhash3::murmurhash3_x86_32(l1_key, 1);
@@ -69,15 +120,6 @@ pub extern "C" fn rx_start(rx_start_args_ptr: *mut c_void) -> i32 {
                 println!("bit count {}", bit_count);
             }
 
-            // parser
-            // create_key(l1 key) l1_cache
-            // create_key(l2 key) lb_filter
-            // write to ring(packet, l2_key)
-            // fib_core_rings[next_core()].enqueue();
-
-            // for i in 0..pkt.len() {
-            //     print!("{:x} ", pkt[i]);
-            // }
         }
         // pp.tx();
     }
