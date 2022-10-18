cad14212e6d32c3d8b843bbcf1f9c498b7964371
K.Tsukimoto <t.29.wuq@gmail.com>
2022-10-18 10:55:11 +0000

    wip config

--- s4dp/src/config.rs
+++ s4dp/src/config.rs
@@ -22,25 +22,30 @@ pub fn parse_switch_args(args: &[String]) -> SwitchConfig {
     opts.optopt("c", "config", "yaml switch config path", "");
 
     let matches = opts.parse(args).unwrap();
-    let config_path: String = matches.opt_get::<String>("p").unwrap().unwrap();
+    let config_path: String = matches.opt_get::<String>("c").unwrap().unwrap();
 
-    let yaml_config = YamlLoader::load_from_str(&fs::read_to_string(config_path).unwrap().to_string()).unwrap()[0];
-    let yaml_general = yaml_config[GENERAL_CONFIG_KEY];
-    let yaml_interfaces = yaml_config[INTERFACES_CONFIG_KEY];
+    let yaml_configs = YamlLoader::load_from_str(&fs::read_to_string(config_path).unwrap().to_string()).unwrap();
+    // let yaml_config = YamlLoader::load_from_str(&fs::read_to_string(config_path).unwrap().to_string()).unwrap()[0];
+    let yaml_config = &yaml_configs[0];
+    let yaml_config_general = &yaml_config[GENERAL_CONFIG_KEY];
+    let yaml_config_interfaces = &yaml_config[INTERFACES_CONFIG_KEY];
 
-    let parser_path = yaml_general["parser_path"].into_string().unwrap();
-    let listen_address = yaml_general["listen_address"].into_string().unwrap();
-    let pipeline_core_num = yaml_general["pipeline_core_num"].as_i64().unwrap() as u8;
+    let parser_path = yaml_config_general["parser_path"].clone().into_string().unwrap();
+    let listen_address = yaml_config_general["listen_address"].clone().into_string().unwrap();
+    let pipeline_core_num = yaml_config_general["pipeline_core_num"].clone().as_i64().unwrap() as u8;
 
-    
-    let interface_configs = Vec::new();
-    for yaml_interface in yaml_interfaces.as_hash().unwrap() {
+    let mut interface_configs = Vec::new();
+    for yaml_config_interface in yaml_config_interfaces.as_hash().unwrap() {
         interface_configs.push(InterfaceConfig{
-            if_name: yaml_interface.0.into_string().unwrap(),
-            cache_core_num: yaml_interface.1["cache_core_num"].as_i64().unwrap() as u8,
+            if_name: yaml_config_interface.0.clone().into_string().unwrap(),
+            cache_core_num: yaml_config_interface.1.clone()["cache_core_num"].as_i64().unwrap() as u8,
         });
-        
     }
 
-    SwitchConfig { parser_path, listen_address, pipeline_core_num, interface_configs }
+    SwitchConfig {
+        parser_path,
+        listen_address,
+        pipeline_core_num,
+        interface_configs
+    }
 }
--- s4dp/src/main.rs
+++ s4dp/src/main.rs
@@ -1,10 +1,10 @@
 // mod dpdk;
 mod core;
-mod controller;
-mod worker;
-mod cache;
-mod fib;
 mod config;
+// mod controller;
+// mod worker;
+// mod cache;
+// mod fib;
 use std::env;
 
 
@@ -15,8 +15,9 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     let switch_args: &[String] = &args[switch_args_start_index as usize..];
     let switch_config = config::parse_switch_args(switch_args);
+    
     // controller start (main core)
-    controller::start_controller(&switch_config);
+    // controller::start_controller(&switch_config);
 
     #[cfg(feature="dpdk")]
     core::helper::dpdk::cleanup();
