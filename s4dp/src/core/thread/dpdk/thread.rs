9ba1d06d312d563c9efe54f9c54c0c5c14a3f628
K.Tsukimoto <t.29.wuq@gmail.com>
2022-10-18 10:55:48 +0000

    wip

--- s4dp/src/core/thread/dpdk/thread.rs
+++ s4dp/src/core/thread/dpdk/thread.rs
@@ -1,11 +1,11 @@
 use std::ffi::c_void;
 
-static CURRENT_LCORE_ID: u32 = u32::MIN;
+static mut CURRENT_LCORE_ID: u32 = u32::MIN;
 
-pub fn spawn(func: fn(*mut c_void) -> i32, args: *mut c_void) -> bool {
+pub fn spawn(func: extern "C" fn(*mut c_void) -> i32, args: *mut c_void) -> bool {
     unsafe {
         let result = dpdk_sys::rte_eal_remote_launch(Some(func), args, CURRENT_LCORE_ID);
         CURRENT_LCORE_ID = dpdk_sys::rte_get_next_lcore(CURRENT_LCORE_ID, 1, 0);
-        result
+        result == 0
     }
 }
