9fca6e71b4e3c3feb7d5f474eb2e4bcc8fc20831
K.Tsukimoto <t.29.wuq@gmail.com>
2023-01-02 19:40:46 +0000

    wip

--- dplane/src/pipeline/tree/avl_tree.rs
+++ dplane/src/pipeline/tree/avl_tree.rs
@@ -17,12 +17,14 @@ pub struct AvlTree {
 
 #[derive(Clone, Copy)]
 struct Node {
+    id: usize,
     parent: Option<usize>,
     left: Option<usize>,
     right: Option<usize>,
     entries: Vector<FlowEntry>,
     value:  Array<u8>,
-    height: u64,
+    r_height: i64,
+    l_height: i64,
 }
 
 
@@ -79,13 +81,15 @@ impl AvlTree {
                 let value = value.unwrap();
 
                 self.nodes.push(Node {
+                    id: self.nodes.len(),
                     parent: None,
                     left: None,
                     right: None,
                     // entries: Vector::new(255, 255),
                     entries: self.heap.vec_malloc(16, 32),
                     value,
-                    height: 0,
+                    r_height: 0,
+                    l_height: 0,
                 });
                 self.root = Some(self.nodes.len() - 1);
                 self.nodes[self.root.unwrap()].entries.push(entry);
@@ -94,93 +98,204 @@ impl AvlTree {
         }
 
 
-        fn add(tree: &mut AvlTree, entry: FlowEntry) -> Option<&Node> {
-            let value = entry.values[tree.key_index].value;
-            if value.is_none() {
-                tree.any_entries.push(entry);
-                return None;
-            }
-            let value = value.unwrap();
+        let value = entry.values[self.key_index].value;
+        if value.is_none() {
+            self.any_entries.push(entry);
+            return;
+        }
+        let value = value.unwrap();
 
-            let nodes_ptr = &mut tree.nodes as *mut Vector<Node>;
+        let nodes_ptr = &mut self.nodes as *mut Vector<Node>;
 
-            unsafe {
-                let mut node = (*nodes_ptr).get(tree.root.unwrap());
+        unsafe {
+            let mut node = (*nodes_ptr).get(self.root.unwrap());
 
-                loop {
-                    let node_value = node.entries[0].values[tree.key_index].value.unwrap();
-                    let mut is_equal = true;
-                    
-                    for i in 0..value.len() {
-                        if value[i] > node_value[i] {
-                            if node.right.is_none() {
-                                tree.nodes.push(Node {
-                                    parent: None,
-                                    left: None,
-                                    right: None,
-                                    // entries: Vector::new(255, 255),
-                                    entries: tree.heap.vec_malloc(16, 32),
-                                    value,
-                                    height: node.height + 1,
-                                });
-                                node.right = Some(tree.nodes.len() - 1);
-                                (*nodes_ptr).get(node.right.unwrap()).entries.push(entry);
-                                return Some(&tree.nodes[node.right.unwrap()]);
-                            }
-                            is_equal = false;
-                            node = (*nodes_ptr).get(node.right.unwrap());
-                            break;
-                        } else if value[i] < node_value[i] {
-                            if node.left.is_none() {
-                                tree.nodes.push(Node {
-                                    parent: None,
-                                    left: None,
-                                    right: None,
-                                    // entries: Vector::new(255, 255),
-                                    entries: tree.heap.vec_malloc(16, 32),
-                                    value,
-                                    height: node.height + 1,
-                                });
-                                node.left = Some(tree.nodes.len() - 1);
-                                tree.nodes[node.left.unwrap()].entries.push(entry);
-                                return Some(&tree.nodes[node.left.unwrap()]);
-                            }
-                            is_equal = false;
-                            node = (*nodes_ptr).get(node.left.unwrap());
-                            break;
+            loop {
+                let node_value = node.entries[0].values[self.key_index].value.unwrap();
+                let mut is_equal = true;
+                
+                for i in 0..value.len() {
+                    if value[i] > node_value[i] {
+                        if node.right.is_none() {
+                            self.nodes.push(Node {
+                                id: self.nodes.len(),
+                                parent: Some(node.id),
+                                left: None,
+                                right: None,
+                                // entries: Vector::new(255, 255),
+                                entries: self.heap.vec_malloc(16, 32),
+                                value,
+                                r_height: 0,
+                                l_height: 0,
+                            });
+                            // node.r_height += 1;
+                            node.right = Some(self.nodes.len() - 1);
+                            (*nodes_ptr).get(node.right.unwrap()).entries.push(entry);
+                            self.rotation(node.right.unwrap());
+                            // return Some(&self.nodes[node.right.unwrap()]);
+                            return;
                         }
+                        is_equal = false;
+                        node = (*nodes_ptr).get(node.right.unwrap());
+                        break;
+                    } else if value[i] < node_value[i] {
+                        if node.left.is_none() {
+                            self.nodes.push(Node {
+                                id: self.nodes.len(),
+                                parent: Some(node.id),
+                                left: None,
+                                right: None,
+                                // entries: Vector::new(255, 255),
+                                entries: self.heap.vec_malloc(16, 32),
+                                value,
+                                r_height: 0,
+                                l_height: 0,
+                            });
+                            // node.l_height += 1;
+                            node.left = Some(self.nodes.len() - 1);
+                            self.nodes[node.left.unwrap()].entries.push(entry);
+                            // return Some(&tree.nodes[node.left.unwrap()]);
+                            self.rotation(node.left.unwrap());
+                            return;
+                        }
+                        is_equal = false;
+                        node = (*nodes_ptr).get(node.left.unwrap());
+                        break;
                     }
+                }
 
-                    if is_equal {
-                        node.entries.push(entry);
-                        return None;
-                    }
+                if is_equal {
+                    node.entries.push(entry);
+                    return;
                 }
             }
         }
 
-
-        let node = add(self, entry);
-        return;
-
-        // if node.is_none() {
-        //     return;
-        // }
-        // let node = node.unwrap();
+    }
 
 
-        // let height_check_node = unsafe {
-        //     if (*node).parent == null_mut() ||
-        //         (*(*node).parent).parent == null_mut() ||
-        //         (*(*(*node).parent).parent).parent == null_mut() {
-        //         return;
-        //     }
-        //     (*(*(*node).parent).parent).parent
-        // };
+    fn rotation(&mut self, start: usize) {
+        let nodes_ptr = &mut self.nodes as *mut Vector<Node>;
 
-        // unsafe {
-        //     if (*height_check_node).left == null_mut() 
-        // }
+        unsafe {
+            let mut parent = (*nodes_ptr).get(self.nodes[start].parent.unwrap());
+            if !parent.right.is_none() && parent.right.unwrap() == start {
+                parent.r_height += 1;
+            } else {
+                parent.l_height += 1;
+            }
+            
+            loop {
+                if parent.parent.is_none() {
+                    self.root = Some(parent.id);
+                    return;
+                }
+                let child_id = parent.id;
+                let child_height = if parent.l_height > parent.r_height {
+                    parent.l_height + 1
+                } else if parent.l_height < parent.r_height {
+                    parent.r_height + 1
+                } else {
+                    parent.r_height + 1
+                };
+                parent = (*nodes_ptr).get(parent.parent.unwrap());
+
+                if !parent.right.is_none() && parent.right.unwrap() == child_id {
+                    parent.r_height = child_height;
+                } else {
+                    parent.l_height = child_height;
+                }
+
+                let diff = parent.l_height - parent.r_height;
+                if diff >= 2 {
+                    let next_parent = (*nodes_ptr).get(parent.left.unwrap());
+                    self.r_rotation(parent.id);
+                    parent = next_parent;
+                } else if diff <= -2 {
+                    let next_parent = (*nodes_ptr).get(parent.right.unwrap());
+                    self.l_rotation(parent.id);
+                    parent = next_parent;
+                }
+            }
+        }
+    }
+
+
+    fn r_rotation(&mut self, start: usize) {
+        let nodes_ptr = &mut self.nodes as *mut Vector<Node>;
+
+        unsafe {
+            let root = (*nodes_ptr).get(start);
+            let new_root = (*nodes_ptr).get(root.left.unwrap());
+            match new_root.right {
+                Some(id) => {
+                    new_root.right = Some(root.id);
+                    let tmp_height = new_root.r_height;
+                    new_root.r_height = root.r_height + 1;
+                    root.l_height = tmp_height;
+                    root.left = Some(id);
+                },
+                None => {
+                    new_root.right = Some(root.id);
+                    new_root.r_height = root.r_height + 1;
+                    root.l_height = 0;
+                }
+            }
+
+
+            if root.parent.is_none() {
+                root.parent = Some(new_root.id);
+                new_root.parent = None;
+                return;
+            }
+            let root_parent = (*nodes_ptr).get(root.parent.unwrap());
+            if !root_parent.right.is_none() && root_parent.right.unwrap() == root.id {
+                root_parent.right = Some(new_root.id);
+            } else {
+                root_parent.left = Some(new_root.id);
+            }
+            new_root.parent = root.parent;
+            root.parent = Some(new_root.id);
+        }
+    }
+
+    fn l_rotation(&mut self, start: usize) {
+        let nodes_ptr = &mut self.nodes as *mut Vector<Node>;
+
+        unsafe {
+            let root = (*nodes_ptr).get(start);
+            let new_root = (*nodes_ptr).get(root.right.unwrap());
+            match new_root.left {
+                Some(id) => {
+                    new_root.left = Some(root.id);
+                    let tmp_height = new_root.l_height;
+                    new_root.l_height = root.l_height + 1;
+                    root.r_height = tmp_height;
+                    root.right = Some(id);
+                },
+                None => {
+                    new_root.left = Some(root.id);
+                    new_root.l_height = root.l_height + 1;
+                    root.r_height = 0;
+                }
+            }
+
+
+            if root.parent.is_none() {
+                root.parent = Some(new_root.id);
+                new_root.parent = None;
+                return;
+            }
+            let root_parent = (*nodes_ptr).get(root.parent.unwrap());
+            if !root_parent.right.is_none() && root_parent.right.unwrap() == root.id {
+                root_parent.right = Some(new_root.id);
+            } else {
+                root_parent.left = Some(new_root.id);
+            }
+            new_root.parent = root.parent;
+            root.parent = Some(new_root.id);
+        }
+
     }
 }
 
@@ -282,6 +397,9 @@ mod tests {
         pkt[2] = 0x0;
         assert_eq!(tree.search(pkt_ptr, 3).len(), 1);
 
+        assert_eq!(tree.nodes[tree.root.unwrap()].r_height, 0);
+        assert_eq!(tree.nodes[tree.root.unwrap()].l_height, 1);
+
         // 0x60, 0x10, 0
         let action_set = ActionSet {
             action_id: 3,
@@ -304,6 +422,9 @@ mod tests {
         assert_eq!(entries.len(), 2);
         assert_eq!(entries[0].action.action_id, 2);
         assert_eq!(entries[1].action.action_id, 3);
+
+        assert_eq!(tree.nodes[tree.root.unwrap()].r_height, 0);
+        assert_eq!(tree.nodes[tree.root.unwrap()].l_height, 1);
 
         // 0xff, 0x10, 0
         let action_set = ActionSet {
@@ -329,5 +450,156 @@ mod tests {
         let entries = tree.search(pkt_ptr, 3);
         assert_eq!(entries.len(), 1);
         assert_eq!(entries[0].action.action_id, 4);
+
+        assert_eq!(tree.nodes[tree.root.unwrap()].r_height, 1);
+        assert_eq!(tree.nodes[tree.root.unwrap()].l_height, 1);
+
+
+        // 0xff, 0x20, 0
+        let action_set = ActionSet {
+            action_id: 5,
+            action_data: Array::new(0),
+        };
+        let mut entry = FlowEntry {
+            values: Array::new(1),
+            priority: 0,
+            action: action_set,
+        };
+        entry.values[0] = MatchFieldValue {
+            value: Some(Array::new(3)),
+            prefix_mask: 0xff, 
+        };
+        entry.values[0].value.unwrap().init(0, 0xff);
+        entry.values[0].value.unwrap().init(1, 0x20);
+        entry.values[0].value.unwrap().init(2, 0);
+        tree.add(entry);
+        let entries = tree.search(pkt_ptr, 3);
+        assert_eq!(entries[0].action.action_id, 4);
+        pkt[0] = 0xff;
+        pkt[1] = 0x20;
+        pkt[2] = 0x0;
+        let entries = tree.search(pkt_ptr, 3);
+        assert_eq!(entries.len(), 1);
+        assert_eq!(entries[0].action.action_id, 5);
+
+
+        assert_eq!(tree.nodes[tree.root.unwrap()].r_height, 2);
+        assert_eq!(tree.nodes[tree.root.unwrap()].l_height, 1);
+
+
+        // 0xff, 0x30, 0
+        let action_set = ActionSet {
+            action_id: 6,
+            action_data: Array::new(0),
+        };
+        let mut entry = FlowEntry {
+            values: Array::new(1),
+            priority: 0,
+            action: action_set,
+        };
+        entry.values[0] = MatchFieldValue {
+            value: Some(Array::new(3)),
+            prefix_mask: 0xff, 
+        };
+        entry.values[0].value.unwrap().init(0, 0xff);
+        entry.values[0].value.unwrap().init(1, 0x30);
+        entry.values[0].value.unwrap().init(2, 0);
+        tree.add(entry);
+        pkt[0] = 0xff;
+        pkt[1] = 0x30;
+        pkt[2] = 0x0;
+        let entries = tree.search(pkt_ptr, 3);
+        assert_eq!(entries.len(), 1);
+        assert_eq!(entries[0].action.action_id, 6);
+
+        assert_eq!(tree.nodes[tree.root.unwrap()].r_height, 2);
+        assert_eq!(tree.nodes[tree.root.unwrap()].l_height, 1);
+
+
+        // 0xff, 0x40, 0
+        let action_set = ActionSet {
+            action_id: 7,
+            action_data: Array::new(0),
+        };
+        let mut entry = FlowEntry {
+            values: Array::new(1),
+            priority: 0,
+            action: action_set,
+        };
+        entry.values[0] = MatchFieldValue {
+            value: Some(Array::new(3)),
+            prefix_mask: 0xff, 
+        };
+        entry.values[0].value.unwrap().init(0, 0xff);
+        entry.values[0].value.unwrap().init(1, 0x40);
+        entry.values[0].value.unwrap().init(2, 0);
+        tree.add(entry);
+        pkt[0] = 0xff;
+        pkt[1] = 0x40;
+        pkt[2] = 0x0;
+        let entries = tree.search(pkt_ptr, 3);
+        assert_eq!(entries.len(), 1);
+        assert_eq!(entries[0].action.action_id, 7);
+
+        assert_eq!(tree.nodes[tree.root.unwrap()].r_height, 2);
+        assert_eq!(tree.nodes[tree.root.unwrap()].l_height, 2);
+
+
+        // 0x50, 0x10, 0
+        let action_set = ActionSet {
+            action_id: 8,
+            action_data: Array::new(0),
+        };
+        let mut entry = FlowEntry {
+            values: Array::new(1),
+            priority: 0,
+            action: action_set,
+        };
+        entry.values[0] = MatchFieldValue {
+            value: Some(Array::new(3)),
+            prefix_mask: 0xff, 
+        };
+        entry.values[0].value.unwrap().init(0, 0x50);
+        entry.values[0].value.unwrap().init(1, 0x10);
+        entry.values[0].value.unwrap().init(2, 0);
+        tree.add(entry);
+        pkt[0] = 0x50;
+        pkt[1] = 0x10;
+        pkt[2] = 0x0;
+        let entries = tree.search(pkt_ptr, 3);
+        assert_eq!(entries.len(), 1);
+        assert_eq!(entries[0].action.action_id, 8);
+
+        assert_eq!(tree.nodes[tree.root.unwrap()].r_height, 2);
+        assert_eq!(tree.nodes[tree.root.unwrap()].l_height, 3);
+
+
+        // 0x40, 0x10, 0
+        let action_set = ActionSet {
+            action_id: 9,
+            action_data: Array::new(0),
+        };
+        let mut entry = FlowEntry {
+            values: Array::new(1),
+            priority: 0,
+            action: action_set,
+        };
+        entry.values[0] = MatchFieldValue {
+            value: Some(Array::new(3)),
+            prefix_mask: 0xff, 
+        };
+        entry.values[0].value.unwrap().init(0, 0x40);
+        entry.values[0].value.unwrap().init(1, 0x10);
+        entry.values[0].value.unwrap().init(2, 0);
+        tree.add(entry);
+        pkt[0] = 0x40;
+        pkt[1] = 0x10;
+        pkt[2] = 0x0;
+        let entries = tree.search(pkt_ptr, 3);
+        assert_eq!(entries.len(), 1);
+        assert_eq!(entries[0].action.action_id, 9);
+
+        assert_eq!(tree.nodes[tree.root.unwrap()].r_height, 2);
+        assert_eq!(tree.nodes[tree.root.unwrap()].l_height, 3);
     }
 }
