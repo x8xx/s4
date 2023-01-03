480495098d8280423f11e97000d6131be95518c1
K.Tsukimoto <t.29.wuq@gmail.com>
2023-01-03 17:47:50 +0000

    wip

--- dplane/src/worker/tx.rs
+++ dplane/src/worker/tx.rs
@@ -26,15 +26,19 @@ pub extern "C" fn start_tx(tx_args_ptr: *mut c_void) -> i32 {
 
     log!("Start Tx{} Core", tx_args.id);
     // let mut pkt_count = 0;
-    let mut d_count = 0; 
-    let mut try_count = 0;
-    let mut next_dequeue_pos = 0;
+    // let mut d_count = 0; 
+    // let mut try_count = 0;
+    // let mut next_dequeue_pos = 0;
     loop {
-        let pktbuf_dequeue_count = tx_args.ring.dequeue_burst_resume::<PktBuf>(&pktbuf_list, next_dequeue_pos, tx_args.batch_count);
-        next_dequeue_pos += pktbuf_dequeue_count;
+        // let pktbuf_dequeue_count = tx_args.ring.dequeue_burst_resume::<PktBuf>(&pktbuf_list, next_dequeue_pos, tx_args.batch_count);
+        let pktbuf_dequeue_count = tx_args.ring.dequeue_burst::<PktBuf>(&pktbuf_list, tx_args.batch_count);
+        for i in 0..pktbuf_dequeue_count {
+            let success_count = tx_args.interface.tx(&mut pktbuf_list[i], 1);
+        }
+        // next_dequeue_pos += pktbuf_dequeue_count;
         // pkt_count += pktbuf_dequeue_count;
         // if next_dequeue_pos >= tx_args.pkt_tx_batch_count || try_count >= 10 {
-        if next_dequeue_pos >= tx_args.pkt_tx_batch_count || true {
+        // if next_dequeue_pos >= tx_args.pkt_tx_batch_count || true {
             // d_count += next_dequeue_pos;
             // if next_dequeue_pos != 0 {
             //     println!("d_count: {}", d_count);
@@ -44,18 +48,18 @@ pub extern "C" fn start_tx(tx_args_ptr: *mut c_void) -> i32 {
             // if d_count == 100000 {
             //     panic!("end");
             // }
-            let success_count = tx_args.interface.tx(&mut pktbuf_list[0], next_dequeue_pos);
-            if success_count != 0 {
-                // log!("Tx{} success send packet {}", tx_args.id, success_count);
-                debug_log!("n pos: {}", next_dequeue_pos);
-                debug_log!("Tx{} success send packet {}", tx_args.id, success_count);
-            }
-            try_count = 0;
-            next_dequeue_pos = 0;
-            continue;
-        }
+            // let success_count = tx_args.interface.tx(&mut pktbuf_list[0], next_dequeue_pos);
+            // if success_count != 0 {
+            //     // log!("Tx{} success send packet {}", tx_args.id, success_count);
+            //     debug_log!("n pos: {}", next_dequeue_pos);
+            //     debug_log!("Tx{} success send packet {}", tx_args.id, success_count);
+            // }
+            // try_count = 0;
+            // next_dequeue_pos = 0;
+            // continue;
+        // }
 
-        try_count += 1;
+        // try_count += 1;
 
         if false {
             return 0;
